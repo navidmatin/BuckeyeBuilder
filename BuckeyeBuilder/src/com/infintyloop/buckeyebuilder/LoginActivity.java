e2a71de45b3272efa0fa8dc52b730f749480e96f
navidmatin <seyedmatin.1@osu.edu>
2014-03-04 16:25:59 -0500

    Add initial login

--- BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/LoginActivity.java
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.infintyloop.buckeyebuilder;
 
+import com.infintyloop.buckeyebuilder.R;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -147,6 +149,8 @@ public class LoginActivity extends Activity {
 			showProgress(true);
 			mAuthTask = new UserLoginTask();
 			mAuthTask.execute((Void) null);
+			Intent intent= new Intent(this, MainActivity.class);
+			startActivity(intent);
 		}
 	}
 
