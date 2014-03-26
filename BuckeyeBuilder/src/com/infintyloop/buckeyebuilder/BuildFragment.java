fe47761b6f703ce4939bee8f319c791488a1959a
Navid Matin <seyedmatin.1@osu.edu>
2014-03-26 15:21:03 -0400

    Updated the fragment, and the map on the main page

--- BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/BuildFragment.java
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/BuildFragment.java
@@ -6,17 +6,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class BuildFragment extends Fragment{
-	public void onCreate(Bundle savedInstanceState)
-	{
-		
-	}
+	final static String ARG_POSITION = "position";
+	int mCurrentPosition = -1;
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		return container;
+		if (savedInstanceState != null) {
+			mCurrentPosition = savedInstanceState.getInt(ARG_POSITION);
+		}
+		return inflater.inflate(R.layout.build_fragment, container,false);
 		
 	}
+	@Override
+	public void onStart()
+	{
+		super.onStart();
+	}
+	@Override
 	public void onPause()
 	{
-	
+		super.onPause();
 	}
+
 }
--- BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/MainActivity.java
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/MainActivity.java
@@ -13,12 +13,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
  		setContentView(R.layout.activity_main);
-		Context context = getApplicationContext();
-		CharSequence text = "Welcome to Initial BuckeyeBuilder! V.000.000.000.01";
-		int duration = Toast.LENGTH_LONG;
-		Toast toast = Toast.makeText(context, text, duration);
-		toast.show();
-		Log.i("OnCreate Called","index");
 	}
 
 	@Override
@@ -30,22 +24,16 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onRestart(){
 		super.onRestart();
-		Context context = getApplicationContext();
-		CharSequence text = "WELCOME BACK";
-		int duration = Toast.LENGTH_LONG;
-		Toast toast = Toast.makeText(context, text, duration);
-		toast.show();
-		Log.i("OnRestart Called","index");
 	}
 	@Override
 	protected void onStop(){
 		super.onStop();
-		Context context = getApplicationContext();
+/*		Context context = getApplicationContext();
 		CharSequence text = "Don't LEAVEEEE!!!!!";
 		int duration = Toast.LENGTH_LONG;
 		Toast toast = Toast.makeText(context, text, duration);
 		toast.show();
-		Log.i("OnStop Called","index");
+		Log.i("OnStop Called","index");*/
 	}
 }
 
