a561e0aab462b5c3fc9e2f35f5fe49f5d446b9ff
navidmatin <seyedmatin.1@osu.edu>
2014-03-26 00:28:08 -0400

    Added Build fragment (Empty fragment)

---
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/Builld.java
@@ -0,0 +1,23 @@
+package com.infintyloop.buckeyebuilder;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Builld extends Fragment{
+	public void onCreate(Bundle savedInstanceState)
+	{
+		
+	}
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		return container;
+		
+	}
+	public void onPause()
+	{
+	
+	}
+}
