ba1363a5c264807c96aac55d15fd9e184c185460
Navid Matin <seyedmatin.1@osu.edu>
2014-03-26 14:49:47 -0400

    Changed Build to BuildFragment

--- BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/BuildFragment.java
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/BuildFragment.java
@@ -1,12 +1,11 @@
 package com.infintyloop.buckeyebuilder;
-
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Builld extends Fragment{
+public class BuildFragment extends Fragment{
 	public void onCreate(Bundle savedInstanceState)
 	{
 		
