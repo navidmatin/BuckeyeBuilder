2ac96e50989fc36083de524152299daaeb83042d
Navid Matin <seyedmatin.1@osu.edu>
2014-03-31 15:28:31 -0400

    Added dialog fragment (incomplete)

--- BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/ManageFragment.java
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/ManageFragment.java
@@ -4,14 +4,17 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class ManageFragment extends Fragment {
+	Bundle bundle;
 	ArrayList<IBuilding> buildingList;
 	LinearLayout linearLayout;
 	@Override
@@ -28,12 +31,34 @@ public class ManageFragment extends Fragment {
 		buildingList=((MainActivity)getActivity()).buildingList;
 		for (IBuilding building : buildingList)
 		{
-			linearLayout = (LinearLayout) getActivity().findViewById(R.id.manage_list);
-			Button button = new Button(getActivity());
-			LayoutParams params = new  LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-			button.setLayoutParams(params);
-			button.setText("Upgrade "+building.GetName());
-			linearLayout.addView(button);	
+			if(building.GetLevel()>0)
+			{
+					//Dynamically adding the buttons
+					linearLayout = (LinearLayout) getActivity().findViewById(R.id.manage_list);
+					Button button = new Button(getActivity());
+					LayoutParams params = new  LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+					button.setLayoutParams(params);
+					button.setText(building.GetName());
+					//Bundling up the building information
+					bundle = new Bundle();
+					bundle.putParcelable("Building", building);
+					
+					button.setOnClickListener(new OnClickListener(){
+		
+						@Override
+						public void onClick(View v) {
+							
+							FragmentManager fm= getFragmentManager();
+							UpgradeDialogFragment upgradeDialog = new UpgradeDialogFragment();
+							upgradeDialog.setArguments(bundle);
+							upgradeDialog.show(fm, "upgrade_dialog_fragment");
+							
+							
+						}
+						
+					});
+					linearLayout.addView(button);	
+			}
 		}
 	}
 	@Override
--- BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/UpgradeDialogFragment.java
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/UpgradeDialogFragment.java
@@ -2,14 +2,54 @@ package com.infintyloop.buckeyebuilder;
 
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class UpgradeDialogFragment extends DialogFragment {
-	static UpgradeDialogFragment newInstance(int num) {
-		UpgradeDialogFragment upgradefragment = new UpgradeDialogFragment();
-		Bundle args = new Bundle();
-		args.putInt("Num", num);
-		upgradefragment.setArguments(args);
+	
+	private TextView buildingName;
+	private TextView description;
+	private TextView level1Cost;
+	private TextView level2Cost;
+	private TextView level3Cost;
+	private TextView level1GenRate;
+	private TextView level2GenRate;
+	private TextView level3GenRate;
+	private Bundle bundle;
+	private IBuilding building;
+	public UpgradeDialogFragment(){
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
+	{
+		View view = inflater.inflate(R.layout.upgrade_dialog_fragment, container);
 		
-		return upgradefragment;
+		buildingName= (TextView) view.findViewById(R.id.upgrade_building_title);
+		description= (TextView) view.findViewById(R.id.upgrade_building_description);
+		level1Cost= (TextView) view.findViewById(R.id.upgrade_level1_cost);
+		level2Cost= (TextView) view.findViewById(R.id.upgrade_level2_cost);
+		level3Cost= (TextView) view.findViewById(R.id.upgrade_level3_cost);
+		level1GenRate= (TextView) view.findViewById(R.id.upgrade_level1_gen_rate);
+		level2GenRate= (TextView) view.findViewById(R.id.upgrade_level2_gen_rate);
+		level3GenRate= (TextView) view.findViewById(R.id.upgrade_level3_gen_rate);
+	
+		return view;	
+	}
+	
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		bundle=this.getArguments();
+		building=bundle.getParcelable("Building");
+		
+		buildingName.setText(building.GetName());
+		description.setText(building.GetDescription());
+		
+		
+		
 	}
 }
