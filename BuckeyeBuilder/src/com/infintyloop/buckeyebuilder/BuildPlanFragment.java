9bce52dcd0a5f39595844b794b64539c698e7d46
kimberlyboydstun <boydstun.3@osu.edu>
2014-04-01 12:06:39 -0400

    finished buildplan stuff, need to create the building detail page next

--- BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/BuildPlanFragment.java
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/BuildPlanFragment.java
@@ -1,7 +1,10 @@
 package com.infintyloop.buckeyebuilder;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +14,18 @@ import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.FragmentTransaction;
 
 public class BuildPlanFragment extends Fragment {
+	ArrayList<IBuilding> buildingList;
 	private Intent intent;
+	LinearLayout linearLayout;
+
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
@@ -26,4 +35,44 @@ public class BuildPlanFragment extends Fragment {
         return rootView;
     }
 
+	@Override
+	public void onStart(){
+		super.onStart();
+		buildingList=((MainActivity)getActivity()).buildingList;
+		for (IBuilding building : buildingList)
+		{
+					linearLayout = (LinearLayout) getActivity().findViewById(R.id.buildplan_list);
+					Button button = new Button(getActivity());
+					LayoutParams params = new  LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+					button.setLayoutParams(params);
+					button.setText(building.GetName());			
+					button.setOnClickListener(showUpgradeFragment(button));
+					linearLayout.addView(button);	
+		}
+	}
+	private View.OnClickListener showUpgradeFragment(final Button button)
+	{
+		return new View.OnClickListener() {
+			@Override
+			public void onClick(View v)
+			{
+				//Bundling up information related to that building
+			//	Bundle bundle = new Bundle();
+			//	bundle.putParcelable("Building", BuildingFactory.FindBuilding((String)button.getText(), buildingList)); //finding building name based on the Button name
+				//Starting up the Dialog Fragment
+			//	FragmentManager fm= getFragmentManager();
+			//	UpgradeDialogFragment upgradeDialog = new UpgradeDialogFragment();
+			//	upgradeDialog.setArguments(bundle);
+			//	upgradeDialog.show(fm, "upgrade_dialog_fragment");
+				
+			}
+		};
+		
+
+	}
+	@Override
+	public void onStop(){
+		super.onPause();
+		linearLayout.removeAllViewsInLayout();
+	}
 }
