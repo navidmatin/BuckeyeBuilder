d09cda62e822961abf7c3ac5a1ace92e1f8cdf27
navidmatin <seyedmatin.1@osu.edu>
2014-04-04 00:11:51 -0400

    Started working on MoneyGenerator

---
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/MoneyGenerator.java
@@ -0,0 +1,19 @@
+package com.infintyloop.buckeyebuilder;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class MoneyGenerator extends IntentService{
+
+	public MoneyGenerator(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
