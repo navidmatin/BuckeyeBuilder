5138ec4604faef219649fa3f2ea19ad3006c5768
Zels123 <tomwallace123@yahoo.com>
2014-04-16 19:58:32 -0400

    building descriptions

--- BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/BuildingFactory.java
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/BuildingFactory.java
@@ -6,18 +6,28 @@ import com.infintyloop.buckeyebuilder.Building;
 import com.infintyloop.buckeyebuilder.IUser;
 
 public class BuildingFactory{
-	private String[] BuildingNames = {"Hitchcock Hall","Bolz Hall","Dreese Laboratories","Scott Laboratories","Caldwell Laboratories", "Koffolt Laboratories","Lincoln Tower","Jennings Hall"};
+	private String[] BuildingNames = {"Hitchcock Hall","Bolz Hall","Dreese Laboratories","Scott Laboratories","Caldwell Laboratories", 
+	 "Koffolt Laboratories","Knowlton Hall","Jennings Hall","Baker Systems"};
+	
 	private int[] Cost = {100,200,300,200,300,400,500,100};
 	private int[] genRates = {20,25,30,25,10,10,70,80};
-	private double[] latitudes = {40.003671,40.002989,40.002319,40.002295,40.002402,2.0,2.0,39.998670};
-	private double[] longitudes = {-83.015363,-83.014996,-83.015826,-83.014377,-83.015043,2.0,2.0,-83.039015};
-	private double[] radiusValues = {0.000087,0.000049,0.000146,0.000378,0.000246,0.0,0.0,0.0005};
-	private String[] description = {"building 1", "building 2", "building 3", "building 4","building 5","building 6", "building 7", "building 8"};
-	private BuildingDatabaseHelper buildingdatabasehelper;
+	private double[] latitudes = {40.003671,40.002989,40.002319,40.002295,40.002402,40.003613,40.003688,39.998670,40.001563};
+	private double[] longitudes = {-83.015363,-83.014996,-83.015826,-83.014377,-83.015043, -83.012501,-83.016863,-83.039015,-83.015875};
+	private double[] radiusValues = {0.000087,0.000049,0.000146,0.000378,0.000246,0.000246,0.000250,0.0005,0.0004};
+	private String[] description = {
+			"Department of Civil and Environmental Engineering, Geodetic Science, Administration, Computer Services and Experiment Station Engineering Building",
+			"College Engineering, College Science, and College Academic Building",
+			"Computer Science and Engineering, Electrical and Computer Engineering Building", 
+			"Department of Mechanical and Aerospace Engineering",
+			"Computer Science and Engineering, Electrical and Computer Engineering Buildin",
+			"Chemical and Biomolecular Engineering", 
+			"Architecture Library, Austin E Knowlton School of Architecture, ksa café", 
+			"Introductory Biology Program",
+			"Integrated Systems Engineering, Office of the Chief Information Officer"
+			};
+	//private BuildingDatabaseHelper buildingdatabasehelper;
 	
 	
-	
-	//Use the database for the above info^
 	public ArrayList<Building> listOfBuildings = new ArrayList<Building>();
 	
 	private int paycheck;
---
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/Testcase1.java
@@ -0,0 +1,8 @@
+package com.infintyloop.buckeyebuilder;
+
+import android.test.AndroidTestCase;
+import junit.framework.TestCase;
+
+public class Testcase1 extends AndroidTestCase {
+
+}
