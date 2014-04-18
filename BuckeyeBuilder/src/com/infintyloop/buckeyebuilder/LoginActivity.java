6c3556ea141ad3dc68c8c2f9159fdc6af9eaa49a
navidmatin <seyedmatin.1@osu.edu>
2014-04-17 23:43:09 -0400

    Quick Fix

--- BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/LoginActivity.java
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/LoginActivity.java
@@ -267,13 +267,6 @@ public class LoginActivity extends Activity {
 				return false;
 			}
 
-			for (String credential : DUMMY_CREDENTIALS) {
-				String[] pieces = credential.split(":");
-				if (pieces[0].equals(musername)) {
-					// Account exists, return true if the password matches.
-					return pieces[1].equals(mPassword);
-				}
-			}
 
 			// TODO: register the new account here.
 			return true;
