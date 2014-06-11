e70ba4fbf6adfd18a6400620c8483266922e5d86
navidmatin <seyedmatin.1@osu.edu>
2014-06-11 17:41:59 -0400

    Added splash screen from http://www.androidhive.info/2013/07/how-to-implement-android-splash-screen-2/ still needs work done

---
+++ BuckeyeBuilder/src/com/infintyloop/buckeyebuilder/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.infintyloop.buckeyebuilder;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+	private static int SPLASH_TIME_OUT = 3000;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		
+		new Handler().postDelayed(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+				startActivity(i);
+				finish();
+			}
+			
+		}, SPLASH_TIME_OUT);
+	}
+}
